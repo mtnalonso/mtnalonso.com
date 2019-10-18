6d26aadb3a03cab1ce61fc6461044aeacfaa7797
Martin Alonso <m13alonso@gmail.com>
2019-10-19 00:16:38 +0200

    Add expandable container functionality

---
+++ js/script.js
@@ -0,0 +1,15 @@
+
+
+function expandContainer(containerButton) {
+    var topContainer = containerButton.parentElement;
+    var parentContainer = topContainer.parentElement;
+    var bottomContainer = parentContainer.lastElementChild;
+
+    if (bottomContainer.style.display === 'none') {
+        bottomContainer.style.display = 'block';
+        containerButton.textContent = '-';
+    } else {
+        bottomContainer.style.display = 'none';
+        containerButton.textContent = '+';
+    }
+}
