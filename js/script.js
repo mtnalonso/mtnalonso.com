1072e9a4ec4841f18b5215f8dd8a3246103e0b91
Martin Alonso <m13alonso@gmail.com>
2019-11-14 01:44:38 +0100

    Add translation and locale files

--- js/script.js
+++ js/script.js
@@ -1,4 +1,31 @@
 
+i18next.use(i18nextXHRBackend).use(i18nextBrowserLanguageDetector).init({
+    fallbackLng: 'en',
+    //debug: true,
+    load: 'languageOnly',
+    backend: {
+        loadPath: '/locales/{{lng}}.json',
+        crossDomain: false
+    }
+}, function(err, t) {
+    updateContent();
+});
+
+function updateContent() {
+    var trnLabels = document.querySelectorAll('.trn');
+    for (var i = 0; i < trnLabels.length; i+=1) {
+        var trnLabel = trnLabels[i];
+        var trnLabelInfo = trnLabel.className.split(' ');
+        var trnLabelKey = trnLabelInfo[1];
+        var trnLabelStr = i18next.t(trnLabelKey);
+
+        if (trnLabelInfo.includes('up')) {
+            trnLabelStr = trnLabelStr.charAt(0).toUpperCase() + trnLabelStr.slice(1);
+        }
+
+        trnLabel.innerHTML = trnLabelStr;
+    }
+}
 
 function expandContainer(containerButton) {
     var topContainer = containerButton.parentElement;
@@ -13,3 +40,5 @@ function expandContainer(containerButton) {
         containerButton.textContent = '+';
     }
 }
+
+
